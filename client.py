6cdc54857c2bf730d8387828c621600e3b5ee50d
ElenaRathsPonce <121831140+ElenaRathsPonce@users.noreply.github.com>
2023-04-13 14:21:29 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import os
 import shutil
+import sys
 from cryptography.fernet import Fernet
+import datetime
+import json
 
 
 class Client:
@@ -18,7 +21,7 @@ class Client:
         self.key = Fernet.generate_key()
 
 
-    # Encrypt the file using the Fernet key
+     # Encrypt the file using the Fernet key
     def encrypt(self, file_path):
         
         with open(file_path, "rb") as f:
@@ -69,7 +72,15 @@ class Client:
                     shutil.copy(upload_path, file_path)
                     # Encrypt the file before uploading
                     self.encrypt(file_path)
-                    print(f"\n{filename} has been uploaded to the subfolder '{os.path.splitext(filename)[0]}' in the 'Uploads' folder.")
+                    print(f"\n{filename} has been successfully uploaded.")
+                    # Save metadata in a file
+                    metadata = {
+                        "filename": filename,
+                        "size": os.path.getsize(file_path),
+                        "upload_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                    with open(os.path.join(subfolder_path, "metadata"), "w") as f:
+                        json.dump(metadata, f)
                     print("Key : ", self.key.decode())
                     break
                 except shutil.Error as e:
@@ -85,7 +96,7 @@ class Client:
                 print(f"- {filename}")
 
 
-    def download_files (self):
+    def download_file(self):
         filename = input("Enter the name of the file to download (include file extension): ")
 
         for root, _, files in os.walk(self.folder_path):
@@ -96,7 +107,7 @@ class Client:
                 download_path = os.path.join(os.path.expanduser("~"), "Downloads", filename)
                 try:
                     shutil.copy(file_path, download_path)
-                    print(f"\n{filename} has been downloaded to '{download_path}'.")
+                    print(f"\n{filename} has been successfully downloaded.")
                 except shutil.Error as e:
                     print(f"Error downloading file: {e}")
                 # Encrypt the file before uploading
@@ -106,10 +117,9 @@ class Client:
             print(f"{filename} could not be found.")
 
 
-
 client = Client()
 
-print("Command options: 'u' to upload a file, 'd' to download a file, 'l' to list all files, 'h' to see additional info\n")
+print("Command options: 'u' to upload a file, 'd' to download a file, 'l' to list all files or 'h' to see additional info or 'exit' to close the program.")
 
 while True:
     user_input = input()
@@ -118,21 +128,27 @@ while True:
         client.upload_file()
 
     elif user_input == 'd':
-        client.download_files()
+        client.download_file()
 
     elif user_input == 'l':
         client.list_files()
-
+    
+    elif user_input == 'exit':
+        print("Exiting program...")
+        sys.exit()
+    
     elif user_input == 'h':
-        print("'u' : All uploaded files are created in the 'Uploads' folder located on the desktop." 
-                   " Within this folder, a subfolder will be created where the uploaded file and its metadata will be stored."
-                   " To upload a file you only have to enter the file name with its extention (e. g. 'upload_file.pdf') and the corresponding access route (e.g. C:\Users\user\Desktop\upload_file.pdf).\n"
-              "'d' : All downloaded files are by default saved in your local 'Downloads' folder."
-                   " To download a file you only have to enter the file name with its extention (e. g. 'download_file.pdf')\n"
-              "'l' : This will display a list of all uploaded files.\n")
-        
+        print("u : All uploaded files are created in the 'Uploads' folder located on the desktop.")
+        print("      Within this folder, a subfolder will be created where the uploaded file and its metadata will be stored.")
+        print("      To upload a file you only have to enter the file name with its extention and the corresponding access route.")
+        print("d : All downloaded files are by default saved in your local 'Downloads' folder.")
+        print("      To download a file you only have to enter the file name with its extention.")
+        print("l : This will display a list of all uploaded files.")
+        print("exit : This will close the program.")
+        print("\nEnter a new command: 'u', 'd', 'l', or 'exit'")
+
     else:
         print("Invalid command")
 
     if user_input != 'h':
-        print("\nEnter 'h' for help, or enter a command ('u', 'd', 'l')")
+        print("\nEnter a new command: 'u', 'd', 'l', 'h' or 'exit'")
